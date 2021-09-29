01a11dc16f6856c4530412980d8e9b455f7290aa
qiqizhang-abukito <zhangqiqi@abukito.com>
2021-09-29 23:20:38 +0800

    leetcode 85 思路

--- week01/maximal_rectangle.go
+++ week01/maximal_rectangle.go
@@ -3,9 +3,25 @@ package week01
 //https://leetcode-cn.com/problems/maximal-rectangle/
 //leetcode 85 最大矩形
 
+
+//思路：
+//row1 1 0 1 0 0            height1 1 0 1 0 0   ==>  利用单调栈取到最大矩形 max1
+//row2 1 0 1 1 1  -----\    height2 2 0 2 1 1   ==>  利用单调栈取到最大矩形 max2
+//row3 1 1 1 1 1  -----/    height3 3 1 3 2 2   ==>  利用单调栈取到最大矩形 max3
+//row4 1 0 0 1 0            height4 4 0 0 3 0   ==>  利用单调栈取到最大矩形 max4
+
+// 由于0的存在导致不连续，所以把问题拆解为从每一行考虑
+// 例如针对row1获取以row1为基础的最大矩形，然后是row2,row3,row4
+// 由于需要取矩形的最大值，考虑用单调栈
+// 如果该行某列上的值为0， 则在这一行计算最大矩形的逻辑中，该列的高度height = 0
+// 根据以上逻辑生成行高的二维数组
+// 针对行高的每一行，调用单调栈(leetcode 84)代码获取在该行下的最大矩形面积
+// 取所有行的最大值，即可得出最终结果
+
 func maximalRectangle(matrix [][]byte) int {
 	var ant int
 	var heights = make([][]int, len(matrix))
+	//生成行高二维数组
 	for i := 0; i < len(matrix); i++ {
 		heights[i] = make([]int, len(matrix[i]))
 		for j := 0; j < len(matrix[i]); j++ {
@@ -22,6 +38,7 @@ func maximalRectangle(matrix [][]byte) int {
 	}
 	// fmt.Printf("height:%v\n", heights)
 	for i := 0; i < len(heights); i++ {
+		//这里直接调用单调栈最大矩形方法
 		var large = LargestRectangleArea(heights[i])
 		if large > ant {
 			ant = large
