0a8d4c8f1a566caa705ca6e52b5bea10696aa5e3
qiqizhang-abukito <zhangqiqi@abukito.com>
2021-10-23 23:16:24 +0800

    week05 some

---
+++ week05/README.md
@@ -0,0 +1,15 @@
+# 算法训练营第五周
+
+## 本周作业
++ [](https://github.com/finger2011/algorithm/blob/master/week05/surrounded_regions.go)
+
+## 实战例题
+
+### 第九课 
+
+#### 二分
+
++ [二分查找](https://github.com/finger2011/algorithm/blob/master/week05/onclass/binary_search.go)
++ [寻找旋转排序数组中的最小值](https://github.com/finger2011/algorithm/blob/master/week05/onclass/find_minimum_in_rotated_sorted_array.go)
+
+
---
+++ week05/onclass/find_minimum_in_rotated_sorted_array_test.go
@@ -0,0 +1,58 @@
+package week05onclass
+
+import "testing"
+
+func Test_findMin(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test01",
+			args: args{
+				nums: []int{3, 4, 5, 1, 2},
+			},
+			want: 1,
+		},
+		{
+			name: "test02",
+			args: args{
+				nums: []int{4, 5, 6, 7, 0, 1, 2},
+			},
+			want: 0,
+		},
+		{
+			name: "test03",
+			args: args{
+				nums: []int{11, 13, 15, 17},
+			},
+			want: 11,
+		},
+		{
+			name: "test04",
+			args: args{
+				nums: []int{1,2},
+			},
+			want: 1,
+		},
+		{
+			name: "test04",
+			args: args{
+				nums: []int{2,1},
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findMin(tt.args.nums); got != tt.want {
+				t.Errorf("findMin() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
